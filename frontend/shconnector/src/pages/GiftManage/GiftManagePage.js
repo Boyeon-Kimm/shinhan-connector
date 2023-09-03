d2b4f57cab26375b99c0a2e0850b65d736eed1a5
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-03 21:03:31 +0900

    Feat: 하단 NavBar로 연결할 페이지 추가 (#18)

---
+++ frontend/shconnector/src/pages/GiftManage/GiftManagePage.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function GiftManagePage() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>선물관리 페이지입니다.</Text>
+    </View>
+  );
+}
