043d221e542bd2a81636902e29cb3cdd9d8ed527
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-06 12:36:54 +0900

    Feat: 스케줄 카드 스켈레톤 완성, css 안함 (#24)

--- frontend/shconnector/src/components/calendar/ScheduleCard.js
+++ frontend/shconnector/src/components/calendar/ScheduleCard.js
@@ -1,9 +1,22 @@
 import { View, Text } from "react-native";
+
+import { FontAwesome } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
-import Button from "../common/Button";
 
-export default function ScheduleCard({ relation, scheduleName, completed }) {
+import Button from "../common/Button";
+import { colors } from "../../config/globalStyles";
+
+export default function ScheduleCard({
+  relation,
+  scheduleName,
+  amount,
+  completed,
+}) {
+  const handleTransfer = () => {
+    // 송금 실행 함수
+  };
+
   return (
     <View>
       <View>
@@ -18,10 +31,28 @@ export default function ScheduleCard({ relation, scheduleName, completed }) {
         <Feather name="more-horizontal" size={24} color="black" />
       </View>
       <View>
-        <Text>[{relation}] {scheduleName}</Text>
+        <Text>
+          [{relation}] {scheduleName}
+        </Text>
+        {completed ? (
+          <View>
+            <Feather name="check" size={24} color="black" />
+            <Text>송금완료</Text>
+          </View>
+        ) : (
+          <View>
+            <FontAwesome name="square-o" size={24} color="black" />
+            <Text>{amount}원</Text>
+          </View>
+        )}
       </View>
       <View>
-        <Button>송금하기</Button> 
+        <Button
+          onPress={handleTransfer}
+          title="송금하기"
+          backgroundColor={colors.shinhan}
+          color="white"
+        ></Button>
         {/* 송금하기 수정 필요 */}
       </View>
     </View>
