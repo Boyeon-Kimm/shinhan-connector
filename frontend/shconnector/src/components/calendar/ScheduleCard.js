1729da5dad5101dba5a83a2d5fe478529bb1a5a5
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-06 09:00:02 +0900

    Feat: 스케줄카드 컴포넌트 작성 중 중간 중간저장 (#24)

--- frontend/shconnector/src/components/calendar/ScheduleCard.js
+++ frontend/shconnector/src/components/calendar/ScheduleCard.js
@@ -1,5 +1,29 @@
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import Button from "../common/Button";
 
-export default function ScheduleCard() {
-  return;
+export default function ScheduleCard({ relation, scheduleName, completed }) {
+  return (
+    <View>
+      <View>
+        <View>
+          <MaterialCommunityIcons
+            name="music-note-whole"
+            size={24}
+            color="black"
+          />
+          <Text>13:00</Text>
+        </View>
+        <Feather name="more-horizontal" size={24} color="black" />
+      </View>
+      <View>
+        <Text>[{relation}] {scheduleName}</Text>
+      </View>
+      <View>
+        <Button>송금하기</Button> 
+        {/* 송금하기 수정 필요 */}
+      </View>
+    </View>
+  );
 }
