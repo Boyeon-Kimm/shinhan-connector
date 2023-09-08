04b2ace6f364e7cb1942edde3e496f8b7efa58a6
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-08 15:18:23 +0900

    Fix: 바텀시트 함수 실행오류 디버깅 완료 (#31)

--- frontend/shconnector/src/pages/Main/MainPage.js
+++ frontend/shconnector/src/pages/Main/MainPage.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import { Fontisto } from '@expo/vector-icons';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
 
-import MyButton from '../../components/common/Button';
-import char1 from '../../../assets/character1.png';
-import char2 from '../../../assets/character2.png';
-import char3 from '../../../assets/character3.png';
-import char7 from '../../../assets/character7.png';
-import char8 from '../../../assets/character8.png';
+import MyButton from "../../components/common/Button";
+import char1 from "../../../assets/character1.png";
+import char2 from "../../../assets/character2.png";
+import char3 from "../../../assets/character3.png";
+import char7 from "../../../assets/character7.png";
+import char8 from "../../../assets/character8.png";
 
 export default function MainPage() {
   return (
@@ -15,11 +15,7 @@ export default function MainPage() {
       <StatusBar style="auto" />
       <View style={styles.titleCon}>
         <Text style={styles.title}>홈</Text>
-        <Fontisto
-          name="bell"
-          size={24}
-          color="black"
-        />
+        <Fontisto name="bell" size={24} color="black" />
       </View>
       <View style={styles.loginCon}>
         <View style={styles.loginUpper}>
@@ -39,11 +35,7 @@ export default function MainPage() {
             />
           </View>
         </View>
-        <MyButton
-          title="로그인"
-          backgroundColor="#2B70CC"
-          color="white"
-        />
+        <MyButton title="로그인" backgroundColor="#2B70CC" color="white" />
       </View>
       <View style={styles.bottom}>
         <View style={styles.titleCon}>
@@ -53,7 +45,7 @@ export default function MainPage() {
           <View style={styles.serviceLine}>
             <TouchableOpacity
               style={styles.serviceEach}
-              onPress={() => navigation.navigate('FriendCreate')}
+              onPress={() => navigation.navigate("FriendCreate")}
             >
               <Text style={styles.serviceTitle}>지인 등록</Text>
               <Image
@@ -99,18 +91,18 @@ export default function MainPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F1F6FD',
-    justifyContent: 'center',
+    backgroundColor: "#F1F6FD",
+    justifyContent: "center",
   },
   titleCon: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginTop: 45,
     marginHorizontal: 35,
   },
   title: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   loginCon: {
     flex: 1.3,
@@ -118,36 +110,36 @@ const styles = StyleSheet.create({
     marginHorizontal: 30,
     paddingHorizontal: 20,
     borderRadius: 15,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   loginUpper: {
     flex: 2,
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingLeft: 12,
   },
   loginImg: {
-    width: '80%',
-    height: '80%',
+    width: "80%",
+    height: "80%",
   },
   loginLeft: {
     flex: 1.5,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   loginRight: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   bottom: {
     flex: 2.5,
   },
   grayText: {
     fontSize: 16,
-    color: 'gray',
+    color: "gray",
   },
   boldText: {
     fontSize: 18,
-    fontWeight: '700',
+    fontWeight: "700",
     letterSpacing: 0.2,
   },
   sub: {
@@ -160,29 +152,29 @@ const styles = StyleSheet.create({
   },
   serviceEach: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 15,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginHorizontal: 10,
     marginBottom: 23,
     paddingHorizontal: 15,
   },
   serviceTitle: {
     fontSize: 18,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   serviceImg: {
-    width: '60%',
-    height: '60%',
+    width: "60%",
+    height: "60%",
     marginLeft: 50,
   },
   serviceLine: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   serviceTitle: {
     fontSize: 16,
-    fontWeight: '600',
+    fontWeight: "600",
     paddingLeft: 5,
     paddingVertical: 10,
   },
