5f1c6e459eee79be5c2c5e3466083b3ba2e22d47
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-10 08:16:03 +0900

    Feat: 지인 목록 리스트 스켈레톤 코드 작성 중 중간저장(#33)

--- frontend/shconnector/src/pages/Friend/FriendPage.js
+++ frontend/shconnector/src/pages/Friend/FriendPage.js
@@ -1,9 +1,76 @@
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, ScrollView, TextInput } from 'react-native';
+import HeaderBar from '../../components/common/HeaderBar';
+import HorizonButton from '../../components/common/HorizonButton';
+import { colors, font, widthScale } from '../../config/globalStyles';
+
+const category = ['전체보기', '가족', '친구', '직장', '거래처', '기타'];
 
 export default function FriendPage() {
+  const [searchCondition, setSearchCondition] = useState(null);
+  const [currentCategory, setCurrentCategory] = useState(category[0]);
+  const handleTextChange = (newText) => {
+    setSearchCondition(newText);
+  };
+
+  const onPressTest = (newCategory) => {
+    setCurrentCategory(newCategory);
+    // console.log('test');
+  };
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>친구목록 페이지입니다.</Text>
+    <View style={styles.container}>
+      <HeaderBar
+        showBackArrow={true}
+        title='지인 목록'
+        showBell={true}
+        onPressRight={null}
+      />
+      <ScrollView style={styles.horizonCon} horizontal={true}>
+        {category.map((item) => (
+          <HorizonButton
+            onPress={()=>{onPressTest(item)}}
+            title={item}
+            backgroundColor={colors.button}
+            color={colors.shinhan}
+            borderColor={colors.button}
+            selected={currentCategory === item ? true : false}
+          />
+        ))}
+      </ScrollView>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleTextChange}
+          placeholder='검색어를 입력해주세요'
+          keyboardType='default'
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    alignItems: 'center',
+  },
+  horizonCon: {
+    // width: '100%',
+    // flexDirection: 'row',
+    // flexWrap: 'nowrap',
+  },
+  input: {
+    fontSize: font(15),
+    borderWidth: 1,
+    borderRadius: 5,
+    // borderColor: '#DCDCDC',
+    height: 50,
+    width: widthScale * 300,
+    padding: 10,
+    marginBottom: 10,
+    borderColor: colors.inputBorder,
+    backgroundColor: colors.inputBackground,
+  },
+});
