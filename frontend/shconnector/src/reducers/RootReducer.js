9052a21c766428943f42565287200711199bed04
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-15 16:48:34 +0900

    Feat: JWT 토큰 저장용 슬라이스 생성 (#58)

--- frontend/shconnector/src/reducers/RootReducer.js
+++ frontend/shconnector/src/reducers/RootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import TestSlice from './TestSlice';
 import CalendarSlice from './CalendarSlice';
+import TokenSlice from './TokenSlice';
 
 const rootReducer = combineReducers({
   test: TestSlice.reducer,
   calendar: CalendarSlice.reducer,
+  token: TokenSlice.reducer,
 });
 
 export default rootReducer;
