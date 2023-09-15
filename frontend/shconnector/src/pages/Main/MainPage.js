3ce23360533a144b216409468495d156e1da6fa1
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-15 23:44:47 +0900

    Feat: 로그인 여부에 따른 상단 컨테이너 내 내용 변경 가능하도록 수정 (#58)

--- frontend/shconnector/src/pages/Main/MainPage.js
+++ frontend/shconnector/src/pages/Main/MainPage.js
@@ -1,96 +1,111 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import { Fontisto } from "@expo/vector-icons";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-import MyButton from "../../components/common/Button";
-import char1 from "../../../assets/character1.png";
-import char2 from "../../../assets/character2.png";
-import char3 from "../../../assets/character3.png";
-import char7 from "../../../assets/character7.png";
-import char8 from "../../../assets/character8.png";
+import { useSelector } from 'react-redux';
+import { StatusBar } from 'expo-status-bar';
+import { Fontisto } from '@expo/vector-icons';
+
+import MyButton from '../../components/common/Button';
+import char1 from '../../../assets/character1.png';
+import char2 from '../../../assets/character2.png';
+import char3 from '../../../assets/character3.png';
+import char7 from '../../../assets/character7.png';
+import char8 from '../../../assets/character8.png';
 
 export default function MainPage({ navigation }) {
+  const name = useSelector((state) => state.login.name);
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
       <View style={styles.titleCon}>
         <Text style={styles.title}>홈</Text>
-        <Fontisto name="bell" size={24} color="black" />
+        <Fontisto name='bell' size={24} color='black' />
       </View>
       <View style={styles.loginCon}>
-        <View style={styles.loginUpper}>
-          <View style={styles.loginLeft}>
-            <View style={styles.sub}>
-              <Text style={styles.grayText}>안녕하세요.</Text>
-              <Text style={styles.grayText}>신한 쏠(SOL) 입니다.</Text>
+        {name ? (
+          <Text>로그인하면 바뀔 것 넣어주세용</Text>
+        ) : (
+          <View style={styles.loginUpper}>
+            <View style={styles.loginLeft}>
+              <View style={styles.sub}>
+                <Text style={styles.grayText}>안녕하세요.</Text>
+                <Text style={styles.grayText}>신한 쏠(SOL) 입니다.</Text>
+              </View>
+              <Text style={styles.boldText}>다양한 서비스 이용을</Text>
+              <Text style={styles.boldText}>위해 로그인 해주세요.</Text>
             </View>
-            <Text style={styles.boldText}>다양한 서비스 이용을</Text>
-            <Text style={styles.boldText}>위해 로그인 해주세요.</Text>
+            <View style={styles.loginRight}>
+              <Image
+                source={char1}
+                resizeMode='contain'
+                style={styles.loginImg}
+              />
+            </View>
           </View>
-          <View style={styles.loginRight}>
-            <Image
-              source={char1}
-              resizeMode="contain"
-              style={styles.loginImg}
-            />
-          </View>
-        </View>
+        )}
+
         <MyButton
-            title="로그인"
-            backgroundColor="#2B70CC"
-            color="white"
-            onPress={() => navigation.navigate("Login")}
-          />
+          title='로그인'
+          backgroundColor='#2B70CC'
+          color='white'
+          onPress={() => navigation.navigate('Login')}
+        />
       </View>
-        <View style={styles.bottom}>
-          <View style={styles.titleCon}>
-            <Text style={styles.title}>지인 관리 서비스</Text>
+      <View style={styles.bottom}>
+        <View style={styles.titleCon}>
+          <Text style={styles.title}>지인 관리 서비스</Text>
+        </View>
+        <View style={styles.serviceCon}>
+          <View style={styles.serviceLine}>
+            <TouchableOpacity
+              style={styles.serviceEach}
+              onPress={() => navigation.navigate('FriendCreate')}
+            >
+              <Text style={styles.serviceTitle}>지인 등록</Text>
+              <Image
+                source={char7}
+                resizeMode='contain'
+                style={styles.serviceImg}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.serviceEach}
+              onPress={() => navigation.navigate('CalendarCreate')}
+            >
+              <Text style={styles.serviceTitle}>일정 등록</Text>
+              <Image
+                source={char8}
+                resizeMode='contain'
+                style={styles.serviceImg}
+              />
+            </TouchableOpacity>
           </View>
-          <View style={styles.serviceCon}>
-            <View style={styles.serviceLine}>
-              <TouchableOpacity
-                style={styles.serviceEach}
-                onPress={() => navigation.navigate("FriendCreate")}
-              >
-                <Text style={styles.serviceTitle}>지인 등록</Text>
-                <Image
-                  source={char7}
-                  resizeMode="contain"
-                  style={styles.serviceImg}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.serviceEach}
-                onPress={() => navigation.navigate("CalendarCreate")}
-              >
-                <Text style={styles.serviceTitle}>일정 등록</Text>
-                <Image
-                  source={char8}
-                  resizeMode="contain"
-                  style={styles.serviceImg}
-                />
-              </TouchableOpacity>
-            </View>
-            <View style={styles.serviceLine}>
-              <TouchableOpacity style={styles.serviceEach} onPress={() => navigation.navigate("Savings")}>
-                <Text style={styles.serviceTitle}>적금편지 상품찾기</Text>
-                <Image
-                  source={char2}
-                  resizeMode="contain"
-                  style={styles.serviceImg}
-                />
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.serviceEach} onPress={() => navigation.navigate("Gift")}>
-                <Text style={styles.serviceTitle}>선물 · 금액 추천</Text>
-                <Image
-                  source={char3}
-                  resizeMode="contain"
-                  style={styles.serviceImg}
-                />
-              </TouchableOpacity>
-            </View>
+          <View style={styles.serviceLine}>
+            <TouchableOpacity
+              style={styles.serviceEach}
+              onPress={() => navigation.navigate('Savings')}
+            >
+              <Text style={styles.serviceTitle}>적금편지 상품찾기</Text>
+              <Image
+                source={char2}
+                resizeMode='contain'
+                style={styles.serviceImg}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.serviceEach}
+              onPress={() => navigation.navigate('Gift')}
+            >
+              <Text style={styles.serviceTitle}>선물 · 금액 추천</Text>
+              <Image
+                source={char3}
+                resizeMode='contain'
+                style={styles.serviceImg}
+              />
+            </TouchableOpacity>
           </View>
         </View>
+      </View>
     </View>
   );
 }
@@ -98,18 +113,18 @@ export default function MainPage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F1F6FD",
-    justifyContent: "center",
+    backgroundColor: '#F1F6FD',
+    justifyContent: 'center',
   },
   titleCon: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginTop: 45,
     marginHorizontal: 35,
   },
   title: {
     fontSize: 24,
-    fontWeight: "600",
+    fontWeight: '600',
   },
   loginCon: {
     flex: 1.3,
@@ -117,36 +132,36 @@ const styles = StyleSheet.create({
     marginHorizontal: 30,
     paddingHorizontal: 20,
     borderRadius: 15,
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   loginUpper: {
     flex: 2,
-    flexDirection: "row",
+    flexDirection: 'row',
     paddingLeft: 12,
   },
   loginImg: {
-    width: "80%",
-    height: "80%",
+    width: '80%',
+    height: '80%',
   },
   loginLeft: {
     flex: 1.5,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   loginRight: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   bottom: {
     flex: 2.5,
   },
   grayText: {
     fontSize: 16,
-    color: "gray",
+    color: 'gray',
   },
   boldText: {
     fontSize: 18,
-    fontWeight: "700",
+    fontWeight: '700',
     letterSpacing: 0.2,
   },
   sub: {
@@ -159,29 +174,29 @@ const styles = StyleSheet.create({
   },
   serviceEach: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 15,
-    justifyContent: "center",
+    justifyContent: 'center',
     marginHorizontal: 10,
     marginBottom: 23,
     paddingHorizontal: 15,
   },
   serviceTitle: {
     fontSize: 18,
-    fontWeight: "600",
+    fontWeight: '600',
   },
   serviceImg: {
-    width: "60%",
-    height: "60%",
+    width: '60%',
+    height: '60%',
     marginLeft: 50,
   },
   serviceLine: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   serviceTitle: {
     fontSize: 16,
-    fontWeight: "600",
+    fontWeight: '600',
     paddingLeft: 5,
     paddingVertical: 10,
   },
