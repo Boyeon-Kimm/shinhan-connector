82d596f5cf771ef59e1389dc5b1b6c630da95ea4
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-16 13:58:43 +0900

    Fix: 선물 추천 페이지 헤더바 교체 완료 (#77)

--- frontend/shconnector/src/pages/Recommend/Gift.js
+++ frontend/shconnector/src/pages/Recommend/Gift.js
@@ -6,35 +6,49 @@ import {
   TouchableOpacity,
   ScrollView,
   TextInput,
-} from "react-native";
-import { StatusBar } from "expo-status-bar";
-import char6 from "../../../assets/character6.png";
-import DropDownPicker from "react-native-dropdown-picker";
-import { useState } from "react";
-import Button from "../../components/common/Button";
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import char6 from '../../../assets/character6.png';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useState } from 'react';
+import Button from '../../components/common/Button';
+import HeaderBar from '../../components/common/HeaderBar';
 
-export default function Gift() {
+export default function Gift({ navigation }) {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    { label: "여성", value: "1" },
-    { label: "남성", value: "2" },
+    { label: '여성', value: '1' },
+    { label: '남성', value: '2' },
   ]);
 
   const [kindOpen, setKindOpen] = useState(false);
   const [kindValue, setKindValue] = useState(null);
   const [kindItems, setKindItems] = useState([
-    { label: "결혼식", value: "1" },
-    { label: "장례식", value: "2" },
-    { label: "잔치", value: "3" },
-    { label: "생일", value: "4" },
-    { label: "집들이", value: "5" },
-    { label: "기타", value: "6" },
+    { label: '결혼식', value: '1' },
+    { label: '장례식', value: '2' },
+    { label: '잔치', value: '3' },
+    { label: '생일', value: '4' },
+    { label: '집들이', value: '5' },
+    { label: '기타', value: '6' },
   ]);
+
+  const handlePressArrow = () => {
+    navigation.goBack();
+  };
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <HeaderBar
+        showBackArrow={true}
+        onPressArrow={handlePressArrow}
+        title={null}
+        showLogout={false}
+        showBell={false}
+        showThreeDots={false}
+        onPressRight={null}
+      />
       <View>
         <Text style={styles.title}>당신을 위한 맞춤 추천</Text>
       </View>
@@ -48,19 +62,19 @@ export default function Gift() {
           </View>
         </View>
         <View style={styles.imgDiv}>
-          <Image source={char6} resizeMode="contain" style={styles.img} />
+          <Image source={char6} resizeMode='contain' style={styles.img} />
         </View>
       </View>
       <View style={styles.inputDiv}>
         <TextInput
           style={styles.input}
-          placeholder="연봉"
-          keyboardType="number-pad"
+          placeholder='연봉'
+          keyboardType='number-pad'
         />
         <TextInput
           style={styles.input}
-          placeholder="연령"
-          keyboardType="number-pad"
+          placeholder='연령'
+          keyboardType='number-pad'
         />
         <View>
           <DropDownPicker
@@ -75,9 +89,9 @@ export default function Gift() {
             setOpen={setOpen}
             setValue={setValue}
             setItems={setItems}
-            placeholder="성별"
+            placeholder='성별'
             modalProps={{
-              animationType: "fade",
+              animationType: 'fade',
             }}
           />
         </View>
@@ -86,7 +100,7 @@ export default function Gift() {
             style={styles.input}
             dropDownContainerStyle={{
               width: 95,
-              borderColor: "#F4F5F7",
+              borderColor: '#F4F5F7',
             }}
             open={kindOpen}
             value={kindValue}
@@ -94,18 +108,18 @@ export default function Gift() {
             setOpen={setKindOpen}
             setValue={setKindValue}
             setItems={setKindItems}
-            placeholder="경조사"
+            placeholder='경조사'
             modalProps={{
-              animationType: "fade",
+              animationType: 'fade',
             }}
           />
         </View>
       </View>
       <View style={styles.btnCon}>
         <Button
-          title="해당 조건으로 검색하기"
-          backgroundColor="#2B70CC"
-          color="white"
+          title='해당 조건으로 검색하기'
+          backgroundColor='#2B70CC'
+          color='white'
         />
       </View>
       <View style={styles.lower}>
@@ -127,34 +141,34 @@ export default function Gift() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   title: {
-    fontWeight: "600",
+    fontWeight: '600',
     fontSize: 20,
     paddingHorizontal: 30,
     paddingBottom: 20,
   },
   blueDiv: {
-    backgroundColor: "#E2EEFF",
-    flexDirection: "row",
+    backgroundColor: '#E2EEFF',
+    flexDirection: 'row',
     paddingHorizontal: 30,
     paddingVertical: 20,
   },
   textDiv: {
     gap: 2,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   boldText: {
     fontSize: 18,
-    fontWeight: "700",
+    fontWeight: '700',
   },
   lowerTextDiv: {
     marginVertical: 10,
     gap: 2,
   },
   btnDiv: {
-    flexDirection: "row",
+    flexDirection: 'row',
     gap: 10,
   },
   img: {
@@ -164,25 +178,25 @@ const styles = StyleSheet.create({
   input: {
     fontSize: 15,
     borderRadius: 5,
-    backgroundColor: "#F4F5F7",
+    backgroundColor: '#F4F5F7',
     height: 50,
     width: 95,
     padding: 10,
   },
   inputDiv: {
-    flexDirection: "row",
-    justifyContent: "center",
-    width: "100%",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
     gap: 5,
-    overflow: "scroll",
+    overflow: 'scroll',
     paddingVertical: 10,
   },
   lower: {
-    alignItems: "center",
+    alignItems: 'center',
   },
   boxShadow: {
     elevation: 10, // 그림자의 강도를 조절할 수 있습니다.
-    backgroundColor: "white", // 그림자를 더 확실하게 보이게 하려면 뷰의 배경색을 설정할 수 있습니다.
+    backgroundColor: 'white', // 그림자를 더 확실하게 보이게 하려면 뷰의 배경색을 설정할 수 있습니다.
     borderRadius: 10, // 뷰의 모서리를 둥글게 만들 수 있습니다.
     padding: 20, // 뷰의 내용과 패딩을 설정합니다.
     width: 300,
