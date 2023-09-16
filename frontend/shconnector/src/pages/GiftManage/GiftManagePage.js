c457783ef0bedcd18b558fa3689b4029ad152d01
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-16 14:24:50 +0900

    Fix: 선물관리 페이지 헤더바 교체 완료 (#77)

--- frontend/shconnector/src/pages/GiftManage/GiftManagePage.js
+++ frontend/shconnector/src/pages/GiftManage/GiftManagePage.js
@@ -1,20 +1,39 @@
-import { Text, View, StyleSheet, ScrollView, TextInput, TouchableOpacity } from "react-native";
-import MyChart from "../../components/giftManage/pieChart";
-import HeaderBar from "../../components/common/HeaderBar";
-import { Shadow } from "react-native-shadow-2";
-import { AntDesign } from "@expo/vector-icons";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import MyChart from '../../components/giftManage/pieChart';
+import HeaderBar from '../../components/common/HeaderBar';
+import { Shadow } from 'react-native-shadow-2';
+import { AntDesign } from '@expo/vector-icons';
 import {
   font,
   statusBarHeight,
   widthScale,
   heightScale,
   colors,
-} from "../../config/globalStyles";
+} from '../../config/globalStyles';
 
-export default function GiftManagePage() {
+export default function GiftManagePage({ navigation }) {
+  const handlePressArrow = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.container}>
-      <HeaderBar showBackArrow={true} showBell={true} title="선물 관리" />
+      <HeaderBar
+        showBackArrow={true}
+        onPressArrow={handlePressArrow}
+        title={'선물 관리'}
+        showLogout={false}
+        showBell={true}
+        showThreeDots={false}
+        onPressRight={null}
+      />
       <View style={styles.chart}>
         <Shadow distance={10} style={styles.shadow}>
           <MyChart ratio={0.7} />
@@ -23,19 +42,19 @@ export default function GiftManagePage() {
       <View style={styles.selectContainer}>
         <View style={styles.select}>
           <Text style={styles.selectText}>전체 선물 내역</Text>
-          <AntDesign name="right" size={18} color="black" />
+          <AntDesign name='right' size={18} color='black' />
         </View>
         <View style={styles.select}>
           <Text style={styles.selectText}>선물 보낸 내역</Text>
-          <AntDesign name="right" size={18} color="black" />
+          <AntDesign name='right' size={18} color='black' />
         </View>
         <View style={styles.select}>
           <Text style={styles.selectText}>선물 받은 내역</Text>
-          <AntDesign name="right" size={18} color="black" />
+          <AntDesign name='right' size={18} color='black' />
         </View>
       </View>
       <AntDesign
-        name="pluscircle"
+        name='pluscircle'
         size={widthScale * 40}
         style={styles.plusButton}
       />
@@ -46,37 +65,42 @@ export default function GiftManagePage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
+    flexDirection: 'column',
+    alignItems: 'center',
     backgroundColor: 'white',
   },
   chart: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     marginTop: 30,
   },
   shadow: {
     borderRadius: 10,
   },
   selectContainer: {
-    flexDirection: "column",
-    width: "100%",
-    alignItems: "center",
+    flexDirection: 'column',
+    width: '100%',
+    alignItems: 'center',
     marginTop: 30,
   },
   select: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     borderBottomWidth: 1,
-    borderBottomColor: "#DCDCDC",
-    width: "80%",
+    borderBottomColor: '#DCDCDC',
+    width: '80%',
     padding: 10,
     paddingTop: 20,
     paddingBottom: 20,
   },
   selectText: {
     fontSize: 15,
-    fontWeight: "600",
+    fontWeight: '600',
   },
-  plusButton: { position: 'absolute', bottom: widthScale * 10, right: widthScale * 20, color: colors.shinhan },
+  plusButton: {
+    position: 'absolute',
+    bottom: widthScale * 10,
+    right: widthScale * 20,
+    color: colors.shinhan,
+  },
 });
