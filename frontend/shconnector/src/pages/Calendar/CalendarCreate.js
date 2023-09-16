89dfb3a85189f22fb25ea3b6be69369c60ecc51c
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-16 13:52:11 +0900

    Fix: 일정 등록 페이지 헤더바 컴포넌트 교체 완료 (#77)

--- frontend/shconnector/src/pages/Calendar/CalendarCreate.js
+++ frontend/shconnector/src/pages/Calendar/CalendarCreate.js
@@ -1,5 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import {
   Text,
   View,
@@ -7,46 +7,47 @@ import {
   StyleSheet,
   TouchableOpacity,
   Switch,
-} from "react-native";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
-import DropDownPicker from "react-native-dropdown-picker";
+} from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import DropDownPicker from 'react-native-dropdown-picker';
+import HeaderBar from '../../components/common/HeaderBar';
 
 Date.prototype.format = function (f) {
-  if (!this.valueOf()) return " ";
+  if (!this.valueOf()) return ' ';
 
   var weekName = [
-    "일요일",
-    "월요일",
-    "화요일",
-    "수요일",
-    "목요일",
-    "금요일",
-    "토요일",
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
   ];
   var d = this;
 
   return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function ($1) {
     switch ($1) {
-      case "yyyy":
+      case 'yyyy':
         return d.getFullYear();
-      case "yy":
+      case 'yy':
         return (d.getFullYear() % 1000).zf(2);
-      case "MM":
+      case 'MM':
         return (d.getMonth() + 1).zf(2);
-      case "dd":
+      case 'dd':
         return d.getDate().zf(2);
-      case "E":
+      case 'E':
         return weekName[d.getDay()];
-      case "HH":
+      case 'HH':
         return d.getHours().zf(2);
-      case "hh":
+      case 'hh':
         return ((h = d.getHours() % 12) ? h : 12).zf(2);
-      case "mm":
+      case 'mm':
         return d.getMinutes().zf(2);
-      case "ss":
+      case 'ss':
         return d.getSeconds().zf(2);
-      case "a/p":
-        return d.getHours() < 12 ? "오전" : "오후";
+      case 'a/p':
+        return d.getHours() < 12 ? '오전' : '오후';
       default:
         return $1;
     }
@@ -54,7 +55,7 @@ Date.prototype.format = function (f) {
 };
 
 String.prototype.string = function (len) {
-  var s = "",
+  var s = '',
     i = 0;
   while (i++ < len) {
     s += this;
@@ -62,26 +63,26 @@ String.prototype.string = function (len) {
   return s;
 };
 String.prototype.zf = function (len) {
-  return "0".string(len - this.length) + this;
+  return '0'.toString(len - this.length) + this;
 };
 Number.prototype.zf = function (len) {
   return this.toString().zf(len);
 };
 
-export default function CalendarCreate() {
+export default function CalendarCreate({navigation}) {
   // 반복 주기 관련
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    { label: "매주", value: "1" },
-    { label: "매달", value: "2" },
-    { label: "매년", value: "3" },
+    { label: '매주', value: '1' },
+    { label: '매달', value: '2' },
+    { label: '매년', value: '3' },
   ]);
 
   // 날짜 및 시간 선택 관련
-  const placeholder = "날짜 및 시간 선택";
+  const placeholder = '날짜 및 시간 선택';
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   const showDatePicker = () => {
     setDatePickerVisibility(true);
   };
@@ -89,9 +90,9 @@ export default function CalendarCreate() {
     setDatePickerVisibility(false);
   };
   const handleConfirm = (date) => {
-    console.warn("dateFormat: ", date.format("yyyy/MM/dd a/p hh:mm"));
+    console.warn('dateFormat: ', date.format('yyyy/MM/dd a/p hh:mm'));
     hideDatePicker();
-    setText(date.format("yyyy/MM/dd a/p hh:mm"));
+    setText(date.format('yyyy/MM/dd a/p hh:mm'));
   };
 
   // 토글 스위치 관련
@@ -103,9 +104,9 @@ export default function CalendarCreate() {
   };
 
   // 알림 시간 관련
-  const timePlaceholder = "— 알림 받을 시간을 선택해주세요 —";
+  const timePlaceholder = '— 알림 받을 시간을 선택해주세요 —';
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
-  const [timeText, setTimeText] = useState("");
+  const [timeText, setTimeText] = useState('');
   const showTimePicker = () => {
     setTimePickerVisibility(true);
   };
@@ -113,37 +114,50 @@ export default function CalendarCreate() {
     setTimePickerVisibility(false);
   };
   const handleTimeConfirm = (date) => {
-    console.warn("dateFormat: ", date.format("a/p hh:mm"));
+    console.warn('dateFormat: ', date.format('a/p hh:mm'));
     hideTimePicker();
-    setTimeText(date.format("a/p hh:mm"));
+    setTimeText(date.format('a/p hh:mm'));
+  };
+
+  const handlePressArrow = () => {
+    navigation.goBack();
   };
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <HeaderBar
+        showBackArrow={true}
+        onPressArrow={handlePressArrow}
+        title={null}
+        showLogout={false}
+        showBell={true}
+        showThreeDots={false}
+        onPressRight={null}
+      />
       <View style={styles.titleCon}>
         <Text style={styles.title}>새로운 일정 추가</Text>
       </View>
-      <TextInput style={styles.input} placeholder="제목" keyboardType="text" />
+      <TextInput style={styles.input} placeholder='제목' keyboardType='text' />
       <TextInput
         style={styles.longInput}
-        placeholder="상세 설명"
-        keyboardType="text"
+        placeholder='상세 설명'
+        keyboardType='text'
       />
       <TouchableOpacity onPress={showDatePicker}>
         <TextInput
-          pointerEvents="none"
+          pointerEvents='none'
           style={styles.input}
           placeholder={placeholder}
-          placeholderTextColor="#BABABA"
-          underlineColorAndroid="transparent"
+          placeholderTextColor='#BABABA'
+          underlineColorAndroid='transparent'
           editable={false}
           value={text}
         />
         <DateTimePickerModal
           headerTextIOS={placeholder}
           isVisible={isDatePickerVisible}
-          mode="datetime"
+          mode='datetime'
           onConfirm={handleConfirm}
           onCancel={hideDatePicker}
         />
@@ -153,7 +167,7 @@ export default function CalendarCreate() {
           style={styles.dropDown}
           dropDownContainerStyle={{
             width: 300,
-            borderColor: "#DCDCDC",
+            borderColor: '#DCDCDC',
           }}
           open={open}
           value={value}
@@ -161,18 +175,18 @@ export default function CalendarCreate() {
           setOpen={setOpen}
           setValue={setValue}
           setItems={setItems}
-          placeholder="— 반복 주기를 선택해주세요 —"
+          placeholder='— 반복 주기를 선택해주세요 —'
           modalProps={{
-            animationType: "fade",
+            animationType: 'fade',
           }}
         />
         <View style={styles.alarmContainer}>
           <View style={styles.alarmContainerHeader}>
             <Text>알림 설정</Text>
             <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
-              thumbColor={isEnabled ? "#ffffff" : "#f4f3f4"}
-              ios_backgroundColor="#3e3e3e"
+              trackColor={{ false: '#767577', true: '#81b0ff' }}
+              thumbColor={isEnabled ? '#ffffff' : '#f4f3f4'}
+              ios_backgroundColor='#3e3e3e'
               onValueChange={toggleSwitch}
               value={isEnabled}
             />
@@ -180,18 +194,18 @@ export default function CalendarCreate() {
           {isAlarmVisible && (
             <TouchableOpacity onPress={showTimePicker}>
               <TextInput
-                pointerEvents="none"
+                pointerEvents='none'
                 style={styles.input}
                 placeholder={timePlaceholder}
-                placeholderTextColor="#BABABA"
-                underlineColorAndroid="transparent"
+                placeholderTextColor='#BABABA'
+                underlineColorAndroid='transparent'
                 editable={false}
                 value={timeText}
               />
               <DateTimePickerModal
                 headerTextIOS={timePlaceholder}
                 isVisible={isTimePickerVisible}
-                mode="time"
+                mode='time'
                 onConfirm={handleTimeConfirm}
                 onCancel={hideTimePicker}
               />
@@ -209,27 +223,27 @@ export default function CalendarCreate() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: "white",
-    alignItems: "center",
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    alignItems: 'center',
   },
   titleCon: {
-    justifyContent: "space-between",
-    alignItems: "center",
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginBottom: 15,
     marginHorizontal: 35,
   },
   title: {
     fontSize: 24,
-    fontWeight: "600",
+    fontWeight: '600',
     marginBottom: 5,
   },
   input: {
     fontSize: 15,
     borderWidth: 1,
     borderRadius: 5,
-    color: "black",
-    borderColor: "#DCDCDC",
+    color: 'black',
+    borderColor: '#DCDCDC',
     height: 50,
     width: 300,
     padding: 10,
@@ -239,48 +253,48 @@ const styles = StyleSheet.create({
     fontSize: 15,
     borderWidth: 1,
     borderRadius: 5,
-    borderColor: "#DCDCDC",
+    borderColor: '#DCDCDC',
     height: 50,
     width: 300,
     padding: 10,
   },
   longInput: {
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "center",
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     borderWidth: 1,
     borderRadius: 5,
-    borderColor: "#DCDCDC",
+    borderColor: '#DCDCDC',
     height: 80,
     width: 300,
     padding: 10,
     marginBottom: 15,
-    textAlignVertical: "top",
+    textAlignVertical: 'top',
   },
   optionText: {
     fontSize: 13,
-    fontweight: "500",
+    fontweight: '500',
     marginBottom: 5,
-    color: "grey",
+    color: 'grey',
   },
   alarmContainer: {
-    flexDirection: "column",
+    flexDirection: 'column',
   },
   alarmContainerHeader: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   submitButton: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: 5,
-    backgroundColor: "#2B70CC",
+    backgroundColor: '#2B70CC',
     width: 300,
     height: 50,
   },
   submitText: {
-    color: "white",
+    color: 'white',
   },
 });
