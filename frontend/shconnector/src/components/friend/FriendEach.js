9e06f430e6e79ebf1a60180c4dd6ce7f8c5b6e3f
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-17 06:41:59 +0900

    Feat: 지인 상세 연결 성공

--- frontend/shconnector/src/components/friend/FriendEach.js
+++ frontend/shconnector/src/components/friend/FriendEach.js
@@ -1,31 +1,50 @@
-import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Image ,TouchableWithoutFeedback} from 'react-native';
 import HeaderBar from '../../components/common/HeaderBar';
 import { StatusBar } from 'expo-status-bar';
 import two from '../../../assets/two.png';
 import Button from '../../components/common/Button';
 import { colors } from '../../config/globalStyles';
+import { useNavigation } from '@react-navigation/native';
 
-export default function FriendEach({relationship, name, group}) {
+export default function FriendEach({
+  relationship,
+  name,
+  group,
+  friendNo,
+  friend,
+}) {
+  const navigation = useNavigation();
   const handlePressArrow = () => {
     navigation.goBack();
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.left}>
-        <View style={styles.imgdiv}>
-          <Image source={two} style={styles.img} />
+    <TouchableWithoutFeedback
+      onPress={()=>navigation.navigate('FriendDetail', {
+        friendNo,
+      })}
+    >
+      <View style={styles.container}>
+        <View style={styles.left}>
+          <View style={styles.imgdiv}>
+            <Image source={two} style={styles.img} />
+          </View>
+          <View style={styles.info}>
+            <Text>[{relationship}]</Text>
+            <Text style={styles.boldtext}>{name}</Text>
+            <Text>{group}</Text>
+          </View>
         </View>
-        <View style={styles.info}>
-          <Text>[{relationship}]</Text>
-          <Text style={styles.boldtext}>{name}</Text>
-          <Text>{group}</Text>
+        <View style={styles.btnCon}>
+          <Button
+            title='송금'
+            backgroundColor={colors.shinhan}
+            color='white'
+            onPress={() => navigation.navigate('CheckAccount', { friend })}
+          />
         </View>
       </View>
-      <View style={styles.btnCon}>
-        <Button title='송금' backgroundColor={colors.shinhan} color='white' />
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -61,5 +80,5 @@ const styles = StyleSheet.create({
   boldtext: {
     fontWeight: '600',
     fontSize: 18,
-  }
+  },
 });
--- frontend/shconnector/src/pages/Friend/FriendDetailPage.js
+++ frontend/shconnector/src/pages/Friend/FriendDetailPage.js
@@ -1,77 +1,92 @@
-import React, { useState, useRef } from "react";
-import { Animated, View, Text, StyleSheet, Image } from "react-native";
-import HeaderBar from "../../components/common/HeaderBar";
-import HorizonButton from "../../components/common/HorizonButton";
-import BottomSheet from "../../components/common/BottomSheet";
-import { useScrollToTop } from "@react-navigation/native";
-import { useCollapsibleHeader } from "react-navigation-collapsible";
-import { Shadow } from "react-native-shadow-2";
+import React, { useState, useRef, useEffect } from 'react';
+import { Animated, View, Text, StyleSheet, Image,TouchableWithoutFeedback } from 'react-native';
+import HeaderBar from '../../components/common/HeaderBar';
+import HorizonButton from '../../components/common/HorizonButton';
+import BottomSheet from '../../components/common/BottomSheet';
+import { useScrollToTop } from '@react-navigation/native';
+import { useCollapsibleHeader } from 'react-navigation-collapsible';
+import { Shadow } from 'react-native-shadow-2';
 
-import { colors, font, screenWidth } from "../../config/globalStyles";
+import { colors, font, screenWidth } from '../../config/globalStyles';
 
-import testImg from "../../../assets/character8.png";
+import testImg from '../../../assets/character8.png';
+import API from '../../util/api';
 
-const category = ["전체보기", "받은 내역", "보낸 내역"];
 
-export default function FriendDetailPage({ friendNo }) {
+const category = ['전체보기', '받은 내역', '보낸 내역'];
+
+export default function FriendDetailPage({ route, navigation }) {
+  const { friendNo } = route.params;
+
+  const [name, setName] = useState(null);
+  const [contact, setContact] = useState(null);
+  const [relation, setRelation] = useState(null);
+  const [belong, setBelong] = useState(null);
+  const [bankCode, setBankCode] = useState(null);
+  const [accountNumber, setAccountNumber] = useState(null);
+
   const [modalVisible, setModalVisible] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(category[0]);
   const handleModify = () => {
-    console.log(friendNo + "수정");
+    console.log(friendNo + '수정');
   };
   const handleDelete = () => {
-    console.log(friendNo + "삭제");
+    console.log(friendNo + '삭제');
   };
 
   const FriendDetailModalData = [
-    { title: "수정", func: handleModify },
-    { title: "삭제", func: handleDelete },
+    { title: '수정', func: handleModify },
+    { title: '삭제', func: handleDelete },
   ];
 
   const data = [
-    { id: 1, name: "아이템1" },
-    { id: 2, name: "아이템2" },
-    { id: 3, name: "아이템3" },
-    { id: 4, name: "아이템4" },
-    { id: 5, name: "아이템5" },
-    { id: 6, name: "아이템6" },
-    { id: 7, name: "아이템7" },
-    { id: 8, name: "아이템8" },
-    { id: 9, name: "아이템9" },
-    { id: 10, name: "아이템10" },
-    { id: 11, name: "아이템11" },
-    { id: 12, name: "아이템12" },
-    { id: 13, name: "아이템13" },
-    { id: 14, name: "아이템14" },
-    { id: 15, name: "아이템15" },
-    { id: 16, name: "아이템15" },
-    { id: 17, name: "아이템15" },
-    { id: 18, name: "아이템15" },
-    { id: 19, name: "아이템15" },
-    { id: 20, name: "아이템15" },
-    { id: 21, name: "아이템15" },
-    { id: 22, name: "아이템15" },
-    { id: 23, name: "아이템15" },
-    { id: 24, name: "아이템15" },
-    { id: 25, name: "아이템15" },
-    { id: 26, name: "아이템15" },
-    { id: 27, name: "아이템15" },
-    { id: 28, name: "아이템15" },
-    { id: 29, name: "아이템15" },
-    { id: 30, name: "아이템15" },
-    { id: 31, name: "아이템15" },
-    { id: 32, name: "아이템15" },
-    { id: 33, name: "아이템15" },
-    { id: 34, name: "아이템15" },
+    { id: 1, name: '아이템1' },
+    { id: 2, name: '아이템2' },
+    { id: 3, name: '아이템3' },
+    { id: 4, name: '아이템4' },
+    { id: 5, name: '아이템5' },
+    { id: 6, name: '아이템6' },
+    { id: 7, name: '아이템7' },
+    { id: 8, name: '아이템8' },
+    { id: 9, name: '아이템9' },
+    { id: 10, name: '아이템10' },
+    { id: 11, name: '아이템11' },
+    { id: 12, name: '아이템12' },
+    { id: 13, name: '아이템13' },
+    { id: 14, name: '아이템14' },
+    { id: 15, name: '아이템15' },
+    { id: 16, name: '아이템15' },
+    { id: 17, name: '아이템15' },
+    { id: 18, name: '아이템15' },
+    { id: 19, name: '아이템15' },
+    { id: 20, name: '아이템15' },
+    { id: 21, name: '아이템15' },
+    { id: 22, name: '아이템15' },
+    { id: 23, name: '아이템15' },
+    { id: 24, name: '아이템15' },
+    { id: 25, name: '아이템15' },
+    { id: 26, name: '아이템15' },
+    { id: 27, name: '아이템15' },
+    { id: 28, name: '아이템15' },
+    { id: 29, name: '아이템15' },
+    { id: 30, name: '아이템15' },
+    { id: 31, name: '아이템15' },
+    { id: 32, name: '아이템15' },
+    { id: 33, name: '아이템15' },
+    { id: 34, name: '아이템15' },
   ];
 
   const onPressHorizon = (newCategory) => {
     setCurrentCategory(newCategory);
   };
 
+  const handlePressArrow = () => {
+    navigation.navigate('FriendPage');
+  };
+
   const onPressThreeDots = () => {
     setModalVisible(true);
-    console.log("three");
+    console.log('three');
   };
 
   const ref = useRef(null);
@@ -88,21 +103,106 @@ export default function FriendDetailPage({ friendNo }) {
 
   const stickyHeaderHeight = 180;
 
+  const bankMapping = {
+    '039': '경남은행',
+    '034': '광주은행',
+    '004': '국민은행',
+    '003': '기업은행',
+    '011': '농협중앙회',
+    '012': '지역농협·축협',
+    '031': '대구은행',
+    102: '대신저축은행',
+    '055': '도이치은행',
+    '052': '모건스탠리은행',
+    '059': '미쓰비시은행',
+    '032': '부산은행',
+    '064': '산림조합중앙회',
+    '002': '산업은행',
+    '050': '상호저축은행',
+    '045': '새마을금고',
+    '007': '수협',
+    '027': '씨티은행',
+    '088': '신한은행',
+    '048': '신협',
+    '060': '아메리카은행',
+    '005': '외환은행',
+    '020': '우리은행',
+    '071': '우체국',
+    105: '웰컴저축은행',
+    '037': '전북은행',
+    '057': '제이피모던체이스은행',
+    '035': '제주은행',
+    '090': '카카오뱅크',
+    '089': '케이뱅크',
+    '081': '하나은행',
+    '008': '한국수출입은행',
+    '001': '한국은행',
+    '054': 'HSBC',
+    '023': 'SC제일은행',
+  };
+
+  const getFriendDetail = async () => {
+    const url = `api/friend/${friendNo}`;
+
+    const response = await API.get(url).catch((error) => {
+      console.error('Axios 에러', error);
+    });
+
+    if (response && response.status === 200) {
+      setName(response.data.name);
+      setContact(response.data.contact);
+      setRelation(response.data.relation);
+      setBelong(response.data.belong);
+      setBankCode(response.data.bankCode);
+      setAccountNumber(response.data.accountNumber);
+    } else {
+      // 모달로 띄울 것
+      console.log('조회 실패');
+    }
+  };
+
+  const handleChangePhone = (text) => {
+    // console.log(text.length);
+    if (text.length > 13) {
+      text.substr(0, 13);
+      return;
+    }
+    let newText = text.replace(/[^0-9]/g, "");
+    if (newText.length < 10)
+      newText = newText
+        .replace(/^(\d{0,3})(\d{0,3})(\d{0,4})$/g, "$1-$2-$3")
+        .replace(/\-{1,2}$/g, "");
+    else
+      newText = newText
+        .replace(/^(\d{0,3})(\d{0,4})(\d{4})$/g, "$1-$2-$3")
+        .replace(/\-{1,2}$/g, "");
+    console.log(newText);
+    setContact(newText);
+  };
+
+  useEffect(() => {
+    getFriendDetail();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Animated.View
         style={{
           transform: [{ translateY }],
-          position: "absolute",
-          backgroundColor: "white",
+          position: 'absolute',
+          backgroundColor: 'white',
           height: stickyHeaderHeight,
-          width: "100%",
+          width: '100%',
           zIndex: 9999,
         }}
       >
-        <Shadow style={{ width: "100%" }}>
+        <Shadow style={{ width: '100%' }}>
           <HeaderBar
             showBackArrow={true}
+            onPressArrow={handlePressArrow}
+            title={null}
+            showLogout={false}
+            showBell={false}
             showThreeDots={true}
             onPressRight={onPressThreeDots}
           />
@@ -111,27 +211,27 @@ export default function FriendDetailPage({ friendNo }) {
             <Text
               style={{
                 fontSize: 17,
-                color: "grey",
-                fontWeight: "600",
+                color: 'grey',
+                fontWeight: '600',
                 marginTop: 30,
                 marginBottom: 20,
               }}
             >
               [직장]
             </Text>
-            <Text style={{ fontSize: 25, fontWeight: "900" }}>김보연</Text>
+            <Text style={{ fontSize: 25, fontWeight: '900' }}>{name}</Text>
             <View style={styles.userInfo}>
               <View style={styles.infoCard}>
                 <Text style={styles.infoText}>전화번호</Text>
-                <Text>010-1234-5678</Text>
+                <Text>{contact}</Text>
               </View>
               <View style={styles.infoCard}>
                 <Text style={styles.infoText}>소속</Text>
-                <Text>신한은행</Text>
+                <Text>{belong}</Text>
               </View>
               <View style={styles.infoCard}>
                 <Text style={styles.infoText}>계좌번호</Text>
-                <Text>[신한] 110-987-654321</Text>
+                <Text>[{bankMapping[bankCode]}] {accountNumber}</Text>
               </View>
             </View>
             <View style={styles.horizonCon} horizontal={true}>
@@ -156,8 +256,8 @@ export default function FriendDetailPage({ friendNo }) {
 
       <Animated.FlatList
         style={{
-          position: "relative",
-          width: "100%",
+          position: 'relative',
+          width: '100%',
         }}
         onScroll={onScroll}
         contentContainerStyle={{
@@ -168,7 +268,7 @@ export default function FriendDetailPage({ friendNo }) {
         }}
         ref={ref}
         data={data}
-        renderItem={(item) => <FriendGiftCard onPressHorizon={null} />}
+        // renderItem={(item) => <FriendGiftCard onPressHorizon={null} />}
         keyExtractor={(item) => item.id}
       />
 
@@ -185,18 +285,18 @@ const imgSize = screenWidth / 3;
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
-    backgroundColor: "white",
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
   headerContent: {
-    flexDirection: "column",
-    alignItems: "center",
+    flexDirection: 'column',
+    alignItems: 'center',
     padding: 10,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     zIndex: 999,
   },
   shadowProp: {
-    shadowColor: "#171717",
+    shadowColor: '#171717',
     shadowOffset: { width: -2, height: 4 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
@@ -209,24 +309,24 @@ const styles = StyleSheet.create({
     borderWidth: font(3),
   },
   horizonCon: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   userInfo: {
-    width: "100%",
-    alignItems: "center",
+    width: '100%',
+    alignItems: 'center',
     gap: 10,
     marginTop: 30,
     marginBottom: 20,
   },
   infoCard: {
     borderWidth: 1,
-    borderColor: "#DCDCDC",
+    borderColor: '#DCDCDC',
     borderRadius: 5,
-    width: "80%",
+    width: '80%',
     padding: 10,
     gap: 5,
   },
   infoText: {
-    color: "grey",
+    color: 'grey',
   },
 });
